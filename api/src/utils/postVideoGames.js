f8c8616750b704b5f399fb5f9f1cff6093615afb
enano17711 <120424316+enano17711@users.noreply.github.com>
2023-04-13 13:37:47 -0400

    fix: server postGame

--- api/src/utils/postVideoGames.js
+++ api/src/utils/postVideoGames.js
@@ -4,6 +4,7 @@ const Genre = db.models.Genre
 const {randomUUID} = require('crypto')
 
 const createVideoGameWithGenres = async (data) => {
+
     try {
         const videoGameDataFormatted = {
             id: randomUUID(),
@@ -18,9 +19,10 @@ const createVideoGameWithGenres = async (data) => {
 
         const videoGame = await VideoGame.create(videoGameDataFormatted)
 
-        for (const genre of data.genres) {
-            const genreOfDataFormatted = await Genre.findByPk(genre.id)
+        for (const genre in data.genres) {
+            const genreOfDataFormatted = await Genre.findByPk(data.genres[genre])
             await videoGame.addGenre(genreOfDataFormatted)
+            const testResult = await VideoGame.findByPk(videoGame.id)
         }
     } catch (e) {
         throw new Error(e.message)
--- front/src/http-common.js
+++ front/src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080",
-    // baseURL: "https://pi-videogames-mdcw.onrender.com/",
+    // baseURL: "http://localhost:8080",
+    baseURL: "https://pi-videogames-mdcw.onrender.com/",
     headers: {
         "Content-type": "application/json"
     }
